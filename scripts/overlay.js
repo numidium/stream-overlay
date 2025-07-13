a88f637352fd469a4b4b911ecb2fce844067fda1
numidium <rbrt.caine@gmail.com>
2025-07-13 04:39:52 -0400

    add cheermote fetch, new cooldown msg

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -1,4 +1,5 @@
 const Queue = require('./queue.js')
+const { StatusCodes } = require('http-status-codes');
 const audioElements = document.querySelectorAll("audio[command]");
 const soundCommands = new Array(audioElements.length);
 const soundCooldowns = {};
@@ -8,26 +9,46 @@ for (let i = 0; i < audioElements.length; i++) {
 
 const alertQueue = new Queue(15);
 const token = (new URLSearchParams(document.location.hash.substring(1))).get("access_token");
-const url = `wss://eventsub.wss.twitch.tv/ws`;
-const eventSocket = new WebSocket(url);
+const eventSocket = new WebSocket("wss://eventsub.wss.twitch.tv/ws");
+const userId = "66293282"; // numidium3rd
 eventSocket.onopen = () => {
     console.log("Socket connected.");
     return false;
 };
 
+let cheermotes;
+(function requestCheermotes() {
+    const req = new XMLHttpRequest();
+    req.open("GET", `https://api.twitch.tv/helix/bits/cheermotes?broadcaster_id=${userId}`, true);
+    //req.open("GET", `http://localhost:3001/helix/bits/cheermotes?broadcaster_id=${userId}`, true);
+    req.setRequestHeader("Client-ID", "dlch9ljsk7ibtvesc4par0knq9gfwz");
+    req.setRequestHeader("Authorization", `Bearer ${token}`);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.onreadystatechange = function () {
+        if (req.readyState == XMLHttpRequest.DONE && req.status >= StatusCodes.OK && req.status < StatusCodes.BAD_REQUEST) {
+            cheermotes = JSON.parse(req.response);
+        }
+    };
+
+    req.send();
+})();
+
 eventSocket.onerror = (error) => {
     console.log(`Socket error: ${error}`);
 };
 
-const followType = "channel.follow";
-const subscribeType = "channel.subscribe";
-const giftType = "channel.subscription.gift";
-const resubType = "channel.subscription.message";
-const chatMessageType = "channel.chat.message";
-const cheerType = "channel.cheer";
-const raidType = "channel.raid";
-const pollBeginType = "channel.poll.begin";
-const pollEndType = "channel.poll.end";
+const subTypes = {
+    follow: "channel.follow",
+    subscribe: "channel.subscribe",
+    gift: "channel.subscription.gift",
+    resub: "channel.subscription.message",
+    chatMessage: "channel.chat.message",
+    cheer: "channel.cheer",
+    raid: "channel.raid",
+    pollBegin: "channel.poll.begin",
+    pollEnd: "channel.poll.end"
+};
+
 let sessionId = null;
 eventSocket.onmessage = (e) => {
     const data = JSON.parse(e.data);
@@ -35,30 +56,30 @@ eventSocket.onmessage = (e) => {
     const messageType = data.metadata.message_type;
     if (messageType === "session_welcome") {
         sessionId = data.payload.session.id;
-        subscribeToEvent(chatMessageType);
-        subscribeToEvent(followType);
-        subscribeToEvent(subscribeType);
-        subscribeToEvent(giftType);
-        subscribeToEvent(resubType);
-        subscribeToEvent(cheerType);
-        subscribeToEvent(raidType);
-        subscribeToEvent(pollBeginType);
-        subscribeToEvent(pollEndType);
+        subscribeToEvent(subTypes.chatMessage);
+        subscribeToEvent(subTypes.follow);
+        subscribeToEvent(subTypes.subscribe);
+        subscribeToEvent(subTypes.gift);
+        subscribeToEvent(subTypes.resub);
+        subscribeToEvent(subTypes.cheer);
+        subscribeToEvent(subTypes.raid);
+        subscribeToEvent(subTypes.pollBegin);
+        subscribeToEvent(subTypes.pollEnd);
     }
     else if (messageType === "notification") {
         const subType = JSON.parse(e.data).payload.subscription.type;
-        if (subType === followType) {
+        if (subType === subTypes.follow) {
             const userName = payloadEvent.user_name;
             alertQueue.enqueue({ alertTitle: `${userName} is now a follower!`, alertMessage: "Greetings!", sound: "follower-sound" });
             startAlertAnims(alertQueue);
         }
-        else if (subType === subscribeType) {
+        else if (subType === subTypes.subscribe) {
             const userName = payloadEvent.user_name;
             const tier = Number(payloadEvent.tier) / 1000;
             alertQueue.enqueue({ alertTitle: `${userName} joined the Mages' Guild!`, alertMessage: tier > 1 ? `Tier ${tier} sub.` : "Welcome!", sound: "spell-sound" });
             startAlertAnims(alertQueue);
         }
-        else if (subType === giftType) {
+        else if (subType === subTypes.gift) {
             const userName = getName(payloadEvent);
             const numGifts = payloadEvent.total;
             const tier = Number(payloadEvent.tier) / 1000;
@@ -66,14 +87,14 @@ eventSocket.onmessage = (e) => {
             alertQueue.enqueue({ alertTitle: `${userName} gifted ${numGifts} ${tierText}subs!`, alertMessage: "Christmas came early!", sound: "subscriber-sound" });
             startAlertAnims(alertQueue);
         }
-        else if (subType === resubType) {
+        else if (subType === subTypes.resub) {
             const userName = payloadEvent.user_name;
             const message = payloadEvent.message.text;
             const cumulativeMonths = Math.floor(payloadEvent.cumulative_months);
             alertQueue.enqueue({ alertTitle: `${userName} has been subbed for ${cumulativeMonths} months total!`, alertMessage: message, sound: "subscriber-sound" });
             startAlertAnims(alertQueue);
         }
-        else if (subType === chatMessageType) {
+        else if (subType === subTypes.chatMessage) {
             const userName = payloadEvent.chatter_user_name;
             const message = payloadEvent.message.text;
             handleSoundCommands(message, userName, soundCooldowns);
@@ -83,23 +104,31 @@ eventSocket.onmessage = (e) => {
                 startAlertAnims(alertQueue);
             }
         }
-        else if (subType === cheerType) {
+        else if (subType === subTypes.cheer) {
+            let message = payloadEvent.message;
+            const cheermoteData = cheermotes.data;
+            for (let i = 0; i < cheermoteData.length; i++) {
+                if (message.indexOf(cheermoteData[i].prefix) === -1)
+                    continue;
+                const markup = `<img src='${cheermoteData[i].tiers[0].images.light.animated["4"]}' />`;
+                message = message.replaceAll(cheermoteData[i].prefix, markup);
+            }
+
             const userName = getName(payloadEvent);
-            const message = payloadEvent.message;
             const bits = Number(payloadEvent.bits);
-            if (bits < 500)
+            if (bits < 1000)
                 alertQueue.enqueue({ alertTitle: `${userName} sent ${bits} bits!`, alertMessage: message, sound: "bits1-sound" });
             else
                 alertQueue.enqueue({ alertTitle: `BIG CHEER! ${userName} sent ${bits} bits!!`, alertMessage: message, sound: "bits2-sound" });
             startAlertAnims(alertQueue);
         }
-        else if (subType === raidType) {
+        else if (subType === subTypes.raid) {
             const userName = payloadEvent.from_broadcaster_user_name;
             const viewers = payloadEvent.viewers;
             alertQueue.enqueue({ alertTitle: `${userName} raided with ${viewers} viewers!`, alertMessage: "Welcome, raiders!", sound: "raid-sound" });
             startAlertAnims(alertQueue);
         }
-        else if (subType === pollBeginType) {
+        else if (subType === subTypes.pollBegin) {
             const voteQuestion = payloadEvent.title;
             const choices = payloadEvent.choices;
             let choicesText = "";
@@ -110,7 +139,7 @@ eventSocket.onmessage = (e) => {
             alertQueue.enqueue({ alertTitle: `Poll started: ${voteQuestion}`, alertMessage: choicesText, sound: "vote-sound" });
             startAlertAnims(alertQueue);
         }
-        else if (subType === pollEndType && payloadEvent.status === "completed") {
+        else if (subType === subTypes.pollEnd && payloadEvent.status === "completed") {
             const voteQuestion = payloadEvent.title;
             const choices = payloadEvent.choices;
             let choicesMarkup = "";
@@ -169,7 +198,6 @@ eventSocket.onclose = (e) => {
 }
 
 function subscribeToEvent(subType) {
-    const userId = "66293282"; // numidium3rd
     const subscription = {
         type: subType,
         version: "1",
@@ -180,25 +208,26 @@ function subscribeToEvent(subType) {
         }
     };
     
-    if (subType === followType) {
+    if (subType === subTypes.follow) {
         subscription.version = "2";
         subscription.condition.moderator_user_id = userId;
     }
-    else if (subType === chatMessageType)
+    else if (subType === subTypes.chatMessage)
         subscription.condition.user_id = userId;
-    if (subType === raidType)
+    if (subType === subTypes.raid)
         subscription.condition.to_broadcaster_user_id = userId;
     else
         subscription.condition.broadcaster_user_id = userId;
 
-    var req = new XMLHttpRequest();
+    const req = new XMLHttpRequest();
     req.open("POST", "https://api.twitch.tv/helix/eventsub/subscriptions", true);
     req.setRequestHeader("Client-ID", "dlch9ljsk7ibtvesc4par0knq9gfwz");
     req.setRequestHeader("Authorization", `Bearer ${token}`);
     req.setRequestHeader("Content-Type", "application/json");
-    req.send(JSON.stringify(subscription));
     req.onreadystatechange = function () {
-    }
+    };
+    
+    req.send(JSON.stringify(subscription));
 }
 
 function playSound(selector) {
@@ -236,7 +265,12 @@ function handleSoundCommands(message, userName, cooldowns) {
         if (cooldown.spamCount >= spamThreshold) {
             cooldown.cooling = true;
             cooldown.time += baseCoolDownTime;
-            alertQueue.enqueue({ alertTitle: `${userName} is on sound timeout for ${cooldown.time / 1000} seconds.`, alertMessage: "Enough!", sound: "enough-sound", image: "enough" });
+            let spamAlerts = [
+                { alertTitle: `${userName} is on sound timeout for ${cooldown.time / 1000} seconds.`, alertMessage: "Enough!", sound: "enough-sound", image: "enough" },
+                { alertTitle: `${userName} is on sound timeout for ${cooldown.time / 1000} seconds.`, alertMessage: "Shut up!", sound: "shutup-sound", image: "shutup" }
+            ];
+
+            alertQueue.enqueue(spamAlerts[Math.floor(Math.random() * 2)]);
             startAlertAnims(alertQueue);
         }
         else {
@@ -253,22 +287,31 @@ function getName(event) {
 }
 
 let isAlertAnimRunning = false;
+
 function startAlertAnims(queue) {
+    const stateEnum = {
+        initialize: 0,
+        fadeIn: 1,
+        stay: 2,
+        fadeOut: 3,
+        end: 4
+    };
+
     if (isAlertAnimRunning)
         return;
     isAlertAnimRunning = true;
+    let timeLast = document.timeline.currentTime;
+    let state = 0;
+    let opacity = 0;
+    let stayTime = 0;
+    const alertElement = document.getElementById("alert-area");
     const fadeTimeLimit = 2000;
     const stayTimeLimit = 5000;
-    let alertElement = document.getElementById("alert-area");
-    let timeLast = document.timeline.currentTime;
-    let opacity = 0;
-    let state = 0;
-    let stayTime = 0;
     function animStep(timeStamp) {
         const timeDelta = timeStamp - timeLast;
         timeLast = timeStamp;
         switch (state) {
-            case 0: // Initialize
+            case stateEnum.initialize:
                 let item = queue.dequeue();
                 document.getElementById("sub-title").textContent = item.alertTitle;
                 document.getElementById("sub-message").innerHTML = item.alertMessage;
@@ -282,7 +325,7 @@ function startAlertAnims(queue) {
                 playSound(`#${item.sound}`);
                 state++;
                 break;
-            case 1: // Fade In
+            case stateEnum.fadeIn:
                 if (opacity < 1) {
                     alertElement.style.opacity = opacity;
                     opacity += timeDelta / fadeTimeLimit;
@@ -293,7 +336,7 @@ function startAlertAnims(queue) {
                 }
 
                 break;
-            case 2: // Stay
+            case stateEnum.stay:
                 if (stayTime < stayTimeLimit) {
                     stayTime += timeDelta;
                 }
@@ -303,7 +346,7 @@ function startAlertAnims(queue) {
                 }
 
                 break;
-            case 3: // Fade Out
+            case stateEnum.fadeOut:
                 if (opacity > 0) {
                     alertElement.style.opacity = opacity;
                     opacity -= timeDelta / fadeTimeLimit;
@@ -314,7 +357,7 @@ function startAlertAnims(queue) {
                 }
 
                 break;
-            case 4: // End
+            case stateEnum.end: // End
                 if (queue.isEmpty()) {
                     isAlertAnimRunning = false;
                     return;
